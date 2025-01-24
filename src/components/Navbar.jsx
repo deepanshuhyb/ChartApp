d23e8d98719e0a7783b8fdadb652ab8fb43c20e3
deepanshuhyb <deepanshuhyb@gmail.com>
2025-01-24 17:57:11 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar () {
+  return (
+    <>
+      <div className='h-20 bg-backgr'>
+        <div className='pt-6'>
+          <h1
+            className='pl-24 text-2xl decoration-red-300
+'
+          >
+            ChartApp
+          </h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
