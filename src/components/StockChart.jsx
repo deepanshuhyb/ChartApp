dd37ba63ccce1b3625d95d7022d3a9c9380d781e
deepanshuhyb <deepanshuhyb@gmail.com>
2025-03-27 02:07:49 +0530

    Netlify _redirects FIX

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Button } from '@/components/ui/button'
 import useStockNames from '@/hooks/useStockNames'
 import loading from '../assets/loader.svg'
 import {
@@ -15,6 +13,7 @@ import {
   Bar
 } from 'recharts'
 import CandlestickChart from './CandlestickChart'
+import { BackgroundBeamsWithCollision } from './ui/background-beams-with-collision'
 
 export default function StockChart () {
   const { id } = useParams()
@@ -104,171 +103,162 @@ export default function StockChart () {
   if (errorInfo) {
     return (
       <>
-        <div className='flex flex-col items-center justify-center min-h-screen bg-[#09090B]'>
-          <div className='relative flex flex-col items-center bg-[#09090B] p-6 rounded-lg shadow-xl w-full max-w-7xl mx-auto text-white'>
-            {errorInfo}
+        <BackgroundBeamsWithCollision>
+          <div className='flex flex-col items-center justify-center w-screen h-[calc(100vh-7.5rem)] md:h-[calc(100vh-9rem)] bg-[#09090B]'>
+            <div className='relative flex flex-col items-center bg-[#09090B] p-6 rounded-lg shadow-xl w-full max-w-7xl mx-auto text-white'>
+              {errorInfo}
+            </div>
           </div>
-        </div>
+        </BackgroundBeamsWithCollision>
       </>
     )
   }
-
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-[#09090B]'>
-      <div className='relative flex flex-col items-center bg-[#09090B] p-6 pt-0 rounded-lg shadow-xl w-full max-w-7xl mx-auto justify-between'>
-        {loader ? (
-          <div className='text-white'>
-            loading...
-            <img src={loading} alt='loading' className='w-12 h-12' />
-          </div>
-        ) : (
-          <>
-            <div className='flex text-lg h-32 pt-sans-narrow-regular pt-4 text-white w-screen md:pl-72 md:pt-8 mb-4 relative md:static md:w-full'>
-              <h2 className='text-white md:text-2xl font-sans text-xl font-semibold px-4 absolute left-12 top-12 md:static md:w-full'>
-                {getStockNameById(id)}
-                <div className='flex gap-1'>
-                  {latestClose !== null && <div>₹{latestClose}</div>}
-                  {profitLoss !== null && (
-                    <div
-                      className={` font-semibold flex items-center justify-center ${
-                        profitLoss >= 0 ? 'text-green-400' : 'text-red-400'
-                      } text-sm sm:text-base md:text-lg`}
-                    >
-                      {profitLoss >= 0
-                        ? `📈 +${profitLoss}%`
-                        : `📉 ${profitLoss}%`}
-                    </div>
-                  )}
-                </div>
-              </h2>
+    <BackgroundBeamsWithCollision>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-[#09090B] w-screen'>
+        <div className='relative flex flex-col items-center bg-[#09090B] p-6 pt-0 rounded-lg shadow-xl w-full max-w-7xl mx-auto justify-between'>
+          {loader ? (
+            <div className='text-white'>
+              loading...
+              <img src={loading} alt='loading' className='w-12 h-12' />
             </div>
-            <div className='w-full max-w-[800px] h-[350px]'>
-              <ResponsiveContainer>
-                {chartType === 'line' ? (
-                  <LineChart
-                    data={stockData}
-                    margin={{ top: 10, right: 10, left: -25, bottom: 5 }}
-                  >
-                    <XAxis
-                      dataKey='time'
-                      stroke='#999999'
-                      tick={{ fontSize: 10, fill: '#dddddd' }}
-                      tickLine={false}
-                    />
-
-                    <YAxis
-                      stroke='#999999'
-                      tick={{ fontSize: 10, fill: '#dddddd' }}
-                      tickLine={false}
-                      // dataKey='auto'
-                      domain={['auto', 'auto']}
-                    />
-                    <Tooltip
-                      contentStyle={{
-                        backgroundColor: '#2a2a2a',
-                        borderRadius: '8px',
-                        border: 'none',
-                        color: '#fff'
-                      }}
-                      labelStyle={{ color: '#C0B283' }}
-                    />
-                    <Line
-                      type='monotone'
-                      dataKey='close'
-                      stroke={lineColor}
-                      strokeWidth={2.25}
-                      dot={false}
-                    />
-                  </LineChart>
-                ) : (
-                  <div className='border-2 w-[850px]  h-[310px] border-white md:block hidden '>
-                    <CandlestickChart candles={candles} />
+          ) : (
+            <>
+              <div className='flex text-lg h-32 pt-sans-narrow-regular pt-4 text-white w-screen md:pl-72 md:pt-8 mb-4 relative md:static md:w-full'>
+                <h2 className='text-white md:text-2xl font-sans text-xl font-semibold px-4 absolute left-12 top-12 md:static md:w-full'>
+                  {getStockNameById(id)}
+                  <div className='flex gap-1'>
+                    {latestClose !== null && <div>₹{latestClose}</div>}
+                    {profitLoss !== null && (
+                      <div
+                        className={` font-semibold flex items-center justify-center ${
+                          profitLoss >= 0 ? 'text-green-400' : 'text-red-400'
+                        } text-sm sm:text-base md:text-lg`}
+                      >
+                        {profitLoss >= 0
+                          ? `📈 +${profitLoss}%`
+                          : `📉 ${profitLoss}%`}
+                      </div>
+                    )}
                   </div>
+                </h2>
+              </div>
+              <div className='w-full max-w-[800px] h-[350px]'>
+                <ResponsiveContainer>
+                  {chartType === 'line' ? (
+                    <LineChart
+                      data={stockData}
+                      margin={{ top: 10, right: 10, left: -25, bottom: 5 }}
+                    >
+                      <XAxis
+                        dataKey='time'
+                        stroke='#999999'
+                        tick={{ fontSize: 10, fill: '#dddddd' }}
+                        tickLine={false}
+                      />
 
-                  // <ComposedChart
-                  //   data={stockData}
-                  //   margin={{ top: 10, right: 10, left: -25, bottom: 5 }}
-                  // >
-                  //   <XAxis
-                  //     dataKey='time'
-                  //     stroke='#999999'
-                  //     tick={{ fill: '#dddddd' }}
-                  //     tickLine={false}
-                  //   />
-                  //   <YAxis
-                  //     stroke='#999999'
-                  //     tick={{ fill: '#dddddd' }}
-                  //     tickLine={false}
-                  //     domain={['auto', 'auto']}
-                  //   />
-                  //   <Tooltip
-                  //     contentStyle={{
-                  //       backgroundColor: '#2a2a2a',
-                  //       borderRadius: '8px',
-                  //       border: 'none',
-                  //       color: '#fff'
-                  //     }}
-                  //     labelStyle={{ color: '#C0B283' }}
-                  //   />
-                  //   <Bar dataKey='close' fill={lineColor} barSize={3} />
-                  // </ComposedChart>
-                )}
-              </ResponsiveContainer>
-            </div>
+                      <YAxis
+                        stroke='#999999'
+                        tick={{ fontSize: 10, fill: '#dddddd' }}
+                        tickLine={false}
+                        // dataKey='auto'
+                        domain={['auto', 'auto']}
+                      />
+                      <Tooltip
+                        contentStyle={{
+                          backgroundColor: '#2a2a2a',
+                          borderRadius: '8px',
+                          border: 'none',
+                          color: '#fff'
+                        }}
+                        labelStyle={{ color: '#C0B283' }}
+                      />
+                      <Line
+                        type='monotone'
+                        dataKey='close'
+                        stroke={lineColor}
+                        strokeWidth={2.25}
+                        dot={false}
+                      />
+                    </LineChart>
+                  ) : (
+                    <div className='border-2 w-[850px]  h-[310px] border-white md:block hidden '>
+                      <CandlestickChart candles={candles} />
+                    </div>
 
-            <div className='justify-center gap-3 flex flex-wrap mt-4'>
-              {timeframes.map(tf => (
+                    // <ComposedChart
+                    //   data={stockData}
+                    //   margin={{ top: 10, right: 10, left: -25, bottom: 5 }}
+                    // >
+                    //   <XAxis
+                    //     dataKey='time'
+                    //     stroke='#999999'
+                    //     tick={{ fill: '#dddddd' }}
+                    //     tickLine={false}
+                    //   />
+                    //   <YAxis
+                    //     stroke='#999999'
+                    //     tick={{ fill: '#dddddd' }}
+                    //     tickLine={false}
+                    //     domain={['auto', 'auto']}
+                    //   />
+                    //   <Tooltip
+                    //     contentStyle={{
+                    //       backgroundColor: '#2a2a2a',
+                    //       borderRadius: '8px',
+                    //       border: 'none',
+                    //       color: '#fff'
+                    //     }}
+                    //     labelStyle={{ color: '#C0B283' }}
+                    //   />
+                    //   <Bar dataKey='close' fill={lineColor} barSize={3} />
+                    // </ComposedChart>
+                  )}
+                </ResponsiveContainer>
+              </div>
+
+              <div className='justify-center gap-3 flex flex-wrap mt-4'>
+                {timeframes.map(tf => (
+                  <button
+                    key={tf.name}
+                    className={`px-3 py-1 border-2 rounded-lg text-white hover:bg-gray-700 ${
+                      timeframe.label === tf.label && timeframe.days === tf.days
+                        ? 'opacity-50'
+                        : ''
+                    }`}
+                    onClick={() =>
+                      setTimeframe({ label: tf.label, days: tf.days })
+                    }
+                  >
+                    {tf.name}
+                  </button>
+                ))}
+              </div>
+
+              <div className='flex flex-row gap-4 pt-4 mt-4'>
                 <button
-                  key={tf.name}
-                  className={`px-3 py-1 border-2 rounded-lg text-white hover:bg-gray-700 ${
-                    timeframe.label === tf.label && timeframe.days === tf.days
-                      ? 'opacity-50'
-                      : ''
+                  className={`px-4 py-2 rounded-lg border-2 font-semibold transition hover:bg-gray-700 text-white ${
+                    chartType === 'line' ? 'opacity-75' : ''
                   }`}
-                  onClick={() =>
-                    setTimeframe({ label: tf.label, days: tf.days })
-                  }
+                  onClick={() => setChartType('line')}
                 >
-                  {tf.name}
+                  Line Chart
                 </button>
-              ))}
-            </div>
+                <button
+                  className={`px-4 py-2 rounded-lg border-2 font-semibold hidden md:block transition hover:bg-gray-700 text-white ${
+                    chartType === 'bar' ? 'opacity-75' : ''
+                  }`}
+                  onClick={() => setChartType('candle')}
+                >
+                  Candlestick data
+                </button>
+              </div>
+            </>
+          )}
+        </div>
 
-            <div className='flex flex-row gap-4 pt-4 mt-4'>
-              <button
-                className={`px-4 py-2 rounded-lg border-2 font-semibold transition hover:bg-gray-700 text-white ${
-                  chartType === 'line' ? 'opacity-75' : ''
-                }`}
-                onClick={() => setChartType('line')}
-              >
-                Line Chart
-              </button>
-              <button
-                className={`px-4 py-2 rounded-lg border-2 font-semibold hidden md:block transition hover:bg-gray-700 text-white ${
-                  chartType === 'bar' ? 'opacity-75' : ''
-                }`}
-                onClick={() => setChartType('candle')}
-              >
-                Candlestick data
-              </button>
-            </div>
-          </>
-        )}
-
-        <div className='text-white'>something</div>
+        <div className='gap-2 flex items-center'></div>
       </div>
-
-      <div className='gap-2 flex items-center'>
-        <Checkbox />
-        <label
-          htmlFor='terms'
-          className='text-white text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-        >
-          Want to learn and get insights?
-        </label>
-        <Button variant='outline'>Yes</Button>
-      </div>
-      <div className='text-white'>a</div>
-    </div>
+    </BackgroundBeamsWithCollision>
   )
 }
